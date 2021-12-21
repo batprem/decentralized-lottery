ca298321587497dd8d352466a59751729875b6d3
pc@zrch.com <pc@zrch.com>
2021-12-22 01:52:11 +0700

    feat: initial contracts

---
+++ contracts/Lottery.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.6;
+
+import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
+
+contract Lottery {
+    address payable[] public players;
+    uint256 public usdEntryFee;
+    AggregatorV3Interface internal ethUsdPriceFeed;
+
+    constructor(address _priceFeedAddress) public {
+        usdEntryFee = 50 * (10**18);
+        ethUsdPriceFeed = AggregatorV3Interface(_priceFeedAddress);
+    }
+
+    function enter() public payable {
+        // $50 minimump
+        players.push(msg.sender);
+    }
+
+    function getEntracnFee() public {}
+
+    function startLottery() public {}
+
+    function endLottery() public {}
+}
