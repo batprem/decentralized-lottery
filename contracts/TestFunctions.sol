066651922f6f4aa8af7e097bfb4cbb60ea1b4c74
pc@zrch.com <pc@zrch.com>
2021-12-25 23:37:58 +0700

    test: Add basic test function

---
+++ contracts/TestFunctions.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.6;
+
+
+contract TestFunctions{
+    function getTestExponential() public view returns (uint256) {
+        return 1e2; // returns 100
+    }
+}
